fa3c9ca547f4bf24696a4818c02ba6f0bc1547f9
jennetd <dickinson.jennet@gmail.com>
2020-12-07 16:35:25 -0600

    add inclusive fit

---
+++ inclusive/output/testModel/build.sh
@@ -0,0 +1,2 @@
+combineCards.py ptbin0pass=ptbin0pass.txt ptbin0fail=ptbin0fail.txt ptbin1pass=ptbin1pass.txt ptbin1fail=ptbin1fail.txt ptbin2pass=ptbin2pass.txt ptbin2fail=ptbin2fail.txt ptbin3pass=ptbin3pass.txt ptbin3fail=ptbin3fail.txt ptbin4pass=ptbin4pass.txt ptbin4fail=ptbin4fail.txt ptbin5pass=ptbin5pass.txt ptbin5fail=ptbin5fail.txt muonCRpass=muonCRpass.txt muonCRfail=muonCRfail.txt > testModel_combined.txt
+text2workspace.py testModel_combined.txt
---
+++ setup.sh
@@ -0,0 +1,3 @@
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc700
+cmsenv
