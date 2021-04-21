e66f2200c98926015998898a43256d19d16c4e3f
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-04-20 19:00:59 -0500

    remove old vbf

--- vbf-threeptbin/expected_significance.sh
+++
@@ -1,3 +0,0 @@
-#combine -M HybridNew output/testModel/testModel_combined.root --LHCmode LHC-significance  --saveToys --fullBToys --saveHybridResult --expectSignal 1 #--verbose 9
-
-combine -M Significance --signif output/testModel/testModel_combined.root --redefineSignalPOIs r --setParameters r=1 -t -1 --verbose 9
